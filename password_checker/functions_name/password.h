2e7287621ebe0703c58a019fb46545d77743ff88
Lubna <lubnadmigdadi@gmail.com>
2026-03-04 23:38:06 +0300

    add functions names

---
+++ password_checker/functions_name/password.h
@@ -0,0 +1,8 @@
+#ifndef PASSWORD_H    
+#define PASSWORD_H
+int uppercase(char password[]);
+int length(char password[]);
+int digits(char password[]);
+int lowercase(char password[]);
+int has_space(char password[]);
+#endif 
